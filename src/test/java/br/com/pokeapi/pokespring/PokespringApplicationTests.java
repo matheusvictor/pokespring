3b93072bcd6ba8134567631bf4ca62a2167a5bf9
Matheus Victor <matheusvictor.salles@gmail.com>
2023-08-25 12:13:57 -0300

    initial commit

---
+++ src/test/java/br/com/pokeapi/pokespring/PokespringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pokeapi.pokespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
