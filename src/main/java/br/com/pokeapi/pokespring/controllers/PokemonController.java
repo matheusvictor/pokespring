fb8ea34cea17695248b495852f3f5466c843fd5b
Matheus Victor <matheusvictor.salles@gmail.com>
2023-08-28 16:14:45 -0300

    refactor: Dependency injection by constructor
    - models package renamed to dto

--- src/main/java/br/com/pokeapi/pokespring/controllers/PokemonController.java
+++ src/main/java/br/com/pokeapi/pokespring/controllers/PokemonController.java
@@ -1,20 +1,24 @@
 package br.com.pokeapi.pokespring.controllers;
 
-import br.com.pokeapi.pokespring.models.Pokemon;
+import br.com.pokeapi.pokespring.dto.PokemonDTO;
 import br.com.pokeapi.pokespring.services.IPokeApiService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/pokemons")
 public class PokemonController {
-
-    @Autowired
     IPokeApiService pokeApiService;
 
+    public PokemonController(IPokeApiService pokeApiService) {
+        this.pokeApiService = pokeApiService;
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<Pokemon> getById(@PathVariable(name = "id") String id) {
+    public ResponseEntity<PokemonDTO> getById(@PathVariable(name = "id") String id) {
         return ResponseEntity.ok(this.pokeApiService.getById(id));
     }
 
