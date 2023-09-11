ce28ff0c5b44c1b254eb43d068637a000fda35c3
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-10 22:38:20 -0300

    feat: Add service to authenticate user

---
+++ src/main/java/br/com/pokeapi/pokespring/services/AuthServiceImpl.java
@@ -0,0 +1,17 @@
+package br.com.pokeapi.pokespring.services;
+
+import br.com.pokeapi.pokespring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthServiceImpl implements IAuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
---
+++ src/main/java/br/com/pokeapi/pokespring/services/IAuthService.java
@@ -0,0 +1,6 @@
+package br.com.pokeapi.pokespring.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface IAuthService extends UserDetailsService {
+}
