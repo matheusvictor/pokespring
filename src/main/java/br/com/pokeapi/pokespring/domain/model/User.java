de1ff17326061084569fad299e4cb45d7652578f
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-10 21:47:57 -0300

    feat: Add user entity

---
+++ src/main/java/br/com/pokeapi/pokespring/domain/model/User.java
@@ -0,0 +1,33 @@
+package br.com.pokeapi.pokespring.domain.model;
+
+import br.com.pokeapi.pokespring.dto.UserData;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "User")
+@Table(name = "USERS")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password_encoded", nullable = false)
+    private String password;
+
+    public User(UserData data) {
+        this.username = data.name();
+        this.password = data.password();
+    }
+
+}
