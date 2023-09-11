93b5565bb113e191d75ca5ca9828d66bdf8d5579
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-10 21:48:29 -0300

    feat: Add user controller
    - method to save new user on DB

---
+++ src/main/java/br/com/pokeapi/pokespring/controllers/UserController.java
@@ -0,0 +1,24 @@
+package br.com.pokeapi.pokespring.controllers;
+
+import br.com.pokeapi.pokespring.dto.UserData;
+import br.com.pokeapi.pokespring.domain.model.User;
+import br.com.pokeapi.pokespring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public void create(@RequestBody UserData data) {
+        userRepository.save(new User(data));
+    }
+
+}
