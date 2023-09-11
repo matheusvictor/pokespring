056cad106f482d60912516eb91155ee34efdfac0
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-11 09:39:58 -0300

    refactor: Update migrations

--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS USERS (
+CREATE TABLE IF NOT EXISTS users (
 
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     password_encoded VARCHAR(50) NOT NULL
 );
+
+INSERT INTO users (id, username, password_encoded) VALUES (1, 'admin', 'admin');
--- src/main/resources/db/migration/V2__insert-admin-on-user-table.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (id, username, password_encoded) VALUES (1, 'admin', 'admin');
