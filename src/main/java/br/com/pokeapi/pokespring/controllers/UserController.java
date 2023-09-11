cbde6d07ee1e1f2bba3af010d3e618590b576d9e
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-10 22:34:07 -0300

    refactor: Add transactional annotation

--- src/main/java/br/com/pokeapi/pokespring/controllers/UserController.java
+++ src/main/java/br/com/pokeapi/pokespring/controllers/UserController.java
@@ -1,9 +1,10 @@
 package br.com.pokeapi.pokespring.controllers;
 
-import br.com.pokeapi.pokespring.dto.UserData;
 import br.com.pokeapi.pokespring.domain.model.User;
+import br.com.pokeapi.pokespring.dto.UserData;
 import br.com.pokeapi.pokespring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class UserController {
     private UserRepository userRepository;
 
     @PostMapping
+    @Transactional
     public void create(@RequestBody UserData data) {
         userRepository.save(new User(data));
     }
